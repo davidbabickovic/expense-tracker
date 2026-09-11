a9a3b7f423ff9e5435b7b8cf301ef541a82d1742
David Babickovic PC <davidbabickovic@gmail.com>
2026-09-11 14:01:59 +0200

    Add edit expense modal

--- static/app.js
+++ static/app.js
@@ -27,6 +27,10 @@ form.addEventListener("submit", async function(event) {
 
     await loadExpenses();
 
+    document.getElementById("description").value = "";
+    document.getElementById("amount").value = "";
+    document.getElementById("paid_by").value = "";  
+
 
 });
 
@@ -68,38 +72,15 @@ async function loadExpenses() {
         editButton.textContent = "Edit";
 
         editButton.addEventListener("click", async function() {
-            const newDescription = prompt("Enter new description:", expense.description);
-            if (newDescription === null) {
-                return; // User cancelled the prompt
-            }
-            const newAmount = parseFloat(prompt("Enter new amount:", expense.amount));
-            if (isNaN(newAmount)) {
-                alert("Invalid amount entered.");
-                return;
-            }
-            const newPaidBy = prompt("Enter new paid by:", expense.paid_by);
-            if (newPaidBy === null) {
-                return; // User cancelled the prompt
-            }
 
-            const updatedExpense = {
-                description: newDescription,
-                amount: newAmount,
-                paid_by: newPaidBy
-            };
-
-            await fetch(`/expenses/${expense.id}`, {
-                method: "PUT",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify(updatedExpense)
-            });
-            
-            await loadExpenses();
-            
-
-            
+            const editFormDiv = document.getElementById("edit-expense-form-div");
+            editFormDiv.style.display = "flex";
+            document.body.style.overflow = "hidden"
+            const editForm = document.getElementById("edit-expense-form");
+            editForm.querySelector("#edit-id").value = expense.id;
+            editForm.querySelector("#edit-description").value = expense.description;
+            editForm.querySelector("#edit-amount").value = expense.amount;
+            editForm.querySelector("#edit-paid_by").value = expense.paid_by;
 
         });
 
@@ -108,6 +89,51 @@ async function loadExpenses() {
         expensesList.appendChild(div);
         
     });
+
+
 }
+
+const editForm = document.getElementById("edit-expense-form");
+
+editForm.addEventListener("submit", async function(event) { 
+
+    event.preventDefault();
+
+    const id = document.getElementById("edit-id").value;
+    const description = document.getElementById("edit-description").value;
+    const amount = parseFloat(document.getElementById("edit-amount").value);
+    const paid_by = document.getElementById("edit-paid_by").value;
+
+    const updatedExpense = {
+        description: description,
+        amount: amount,
+        paid_by: paid_by
+    };
+
+    await fetch(`/expenses/${id}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(updatedExpense)
+    });
+
+    await loadExpenses();
+    const editFormDiv = document.getElementById("edit-expense-form-div");
+    editFormDiv.style.display = "none";
+    document.body.style.overflow = ""
+
+
+});
+
+const editFormCancelButton = document.getElementById("edit-cancel-button");
+
+editFormCancelButton.addEventListener("click", function() {
+    const editFormDiv = document.getElementById("edit-expense-form-div");
+    editFormDiv.style.display = "none";
+    document.body.style.overflow = ""
+    
+});
+
 
 loadExpenses();
